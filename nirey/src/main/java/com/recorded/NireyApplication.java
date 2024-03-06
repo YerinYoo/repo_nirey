9c683e5bb6cff4136fe9e6c11416984f90f9afa4
YerinYoo <ooyo.che1s@gmail.com>
2024-03-06 15:44:41 +0900

    first

---
+++ nirey/src/main/java/com/recorded/NireyApplication.java
@@ -0,0 +1,13 @@
+package com.recorded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NireyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NireyApplication.class, args);
+	}
+
+}
---
+++ nirey/src/test/java/com/recorded/NireyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recorded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NireyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
