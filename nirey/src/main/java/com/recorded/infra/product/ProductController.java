5d6bf70b62abc751d51ceba0bb9edaffe2207836
YerinYoo <ooyo.che1s@gmail.com>
2024-04-14 23:56:28 +0900

    24.04.14

--- nirey/src/main/java/com/recorded/infra/product/ProductController.java
+++ nirey/src/main/java/com/recorded/infra/product/ProductController.java
@@ -86,21 +86,22 @@ public class ProductController {
 		return "redirect:/Porders";
 	}
 
-//	@RequestMapping(value = "/Porders")
-//	public String Morders(@ModelAttribute("vo") ProductVo vo, Model model) throws Exception {
-//
-//		setSearch(vo);
-//		int rowcount = service.getTotalProductCount(vo);
-//		model.addAttribute("listCount", rowcount);
-//
-//		System.out.println(rowcount);
-//
-//		model.addAttribute("list", service.selectList(vo));
-//
-////		 model.addAttribute("vo", vo);
-//
-//		return "adm/infra/v1/Porders";
-//	}
+	@RequestMapping(value = "/Porders")
+	public String Morders(@ModelAttribute("vo") ProductVo vo, Model model) throws Exception {
+		
+		setSearch(vo, null, null);
+		int rowcount = service.getTotalProductCount(vo);
+		model.addAttribute("listCount", rowcount);
+		
+		System.out.println(rowcount);
+		
+		model.addAttribute("list", service.selectList(vo));
+		
+//		 model.addAttribute("vo", vo);
+		
+		return "adm/infra/v1/Porders";
+	}
+	
 
 	@RequestMapping(value = "/PordersView")
 	public String PordersView(ProductDto dto, Model model) throws Exception {
@@ -126,14 +127,6 @@ public class ProductController {
 	}
 
 	public void setSearch(ProductVo vo, String string, String userSeq) throws Exception {
-		/*
-		 * 최초 화면 로딩시에 세팅은 문제가 없지만 /* 이후 전체적으로 데이터를 조회를 하려면 null 값이 넘어 오는 관계로 문제가 전체 데이터
-		 * 조회가 되지 못한다.
-		 */
-		/* 해서 BaseVo.java 에서 기본값을 주어서 처리 */
-//		vo.setShUseNy(vo.getShUseNy() == null ? 1 : vo.getShUseNy());
-//		vo.setShDelNy(vo.getShDelNy() == null ? 0 : vo.getShDelNy());
-//		vo.setShOptionDate(vo.getShOptionDate() == null ? 2 : vo.getShOptionDate());
 
 		/* 초기값 세팅이 있는 경우 사용 */
 		vo.setShDateStart(vo.getShDateStart() == null
@@ -142,12 +135,6 @@ public class ProductController {
 		vo.setShDateEnd(
 				vo.getShDateEnd() == null ? UtilDateTime.nowString() : UtilDateTime.add59TimeString(vo.getShDateEnd()));
 	}
-
-//		/* 초기값 세팅이 없는 경우 사용 */
-//		vo.setShDateStart(vo.getShDateStart() == null || vo.getShDateStart() == "" ? null : UtilDateTime.add00TimeString(vo.getShDateStart()));
-//		vo.setShDateEnd(vo.getShDateEnd() == null || vo.getShDateEnd() == "" ? null : UtilDateTime.add59TimeString(vo.getShDateEnd()));
-
-	// 사용자 페이지 관련 s
 
 	@RequestMapping(value = "/recorded")
 	public String recorded(@ModelAttribute("vo") ProductVo vo, HttpSession session, Model model) throws Exception {
