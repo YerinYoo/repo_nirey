d027a34f059037af90e622839e0cbba80a15c86c
YerinYoo <155711801+YerinYoo@users.noreply.github.com>
2024-05-02 12:40:20 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # start.sh
 
-PROJECT="mia-0.0.1"
+PROJECT="meomy-0.0.1"
 PROJECT_FULL_PATH_01="/home/ec2-user/service/recorded-prdt-01"
 JAR_FILE="$PROJECT_FULL_PATH_01/$PROJECT.jar"
 LOG_PATH="$PROJECT_FULL_PATH_01/logs"
