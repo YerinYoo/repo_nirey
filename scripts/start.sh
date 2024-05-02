9e9e35543b1bdbf67c383c8d0a50881239d8ad21
YerinYoo <155711801+YerinYoo@users.noreply.github.com>
2024-05-02 17:21:58 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 # start.sh
 
 PROJECT="nirey-0.0.1"
-PROJECT_FULL_PATH_01="/home/ec2-user/service/recorded-prdt" 
+PROJECT_FULL_PATH_01="/home/ec2-user/service/recorded-prdt"
 JAR_FILE="$PROJECT_FULL_PATH_01/$PROJECT.jar"
 LOG_PATH="$PROJECT_FULL_PATH_01/logs"
 
